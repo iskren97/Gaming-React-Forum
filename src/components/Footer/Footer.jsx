f8aafcf8e07efe65d1c5ea81f896c983d1e7ec86
Iskren Gyorev <iskrengyorev@gmail.com>
2022-04-29 19:42:47 +0300

    Added dividers in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
+import { Divider } from '@mui/material';
 
 import InfoIcon from '@mui/icons-material/Info';
 import ContactPageIcon from '@mui/icons-material/ContactPage';
@@ -18,6 +19,8 @@ const Footer = () => {
       <Grid container spacing={2} direction="row" className="footer-grid">
         <Grid item xs={3}>
           <h3>About</h3>
+          <Divider sx={{ bgcolor: '#47DB00' }} variant={'middle'} />
+
           <a>
             <InfoIcon style={{ marginTop: '6px' }} />
           </a>
@@ -26,6 +29,8 @@ const Footer = () => {
 
         <Grid item xs={3}>
           <h3>Contact</h3>
+          <Divider sx={{ bgcolor: '#47DB00' }} variant={'middle'} />
+
           <a>
             <ContactPageIcon style={{ marginTop: '6px' }} />
           </a>
@@ -34,6 +39,8 @@ const Footer = () => {
 
         <Grid item xs={3}>
           <h3>Support</h3>
+          <Divider sx={{ bgcolor: '#47DB00' }} variant={'middle'} />
+
           <a>
             <RecommendIcon style={{ marginTop: '6px' }} />
           </a>
@@ -42,6 +49,8 @@ const Footer = () => {
 
         <Grid item xs={3}>
           <h3>Socials Follow</h3>
+          <Divider sx={{ bgcolor: '#47DB00' }} variant={'middle'} />
+
           <Grid
             container
             spacing={2}
