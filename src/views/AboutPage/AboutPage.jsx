daa34f0e297d6895ee0c7f31f70fea3f28ffb49d
Iskren Gyorev <iskrengyorev@gmail.com>
2022-05-01 18:25:57 +0300

    Improved about view and provided data

--- src/views/AboutPage/AboutPage.jsx
+++ src/views/AboutPage/AboutPage.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import Profile from '../Profile/Profile';
+import img1 from '../Profile/imgs/iskren.JPG';
+import img2 from '../Profile/imgs/georgi.JPG';
 
 import Rating from '@mui/material/Rating';
 
@@ -11,23 +13,43 @@ const AboutPage = () => {
   return (
     <Grid container spacing={0} direction="row">
       <Grid item xs={4}>
-        <Profile />
+        <Profile
+          name="Iskren Gyorev"
+          img={img1}
+          age="24"
+          location="https://www.google.com/maps/place/%D0%92%D0%B8%D0%B4%D0%B8%D0%BD/@43.9796191,22.8423547,13z/data=!3m1!4b1!4m5!3m4!1s0x475379eca2009025:0x1f853ae2f877d8b7!8m2!3d43.996159!4d22.8679302"
+          town="Vidin"
+          ins="https://www.instagram.com/iskren_e/"
+          fb="https://www.facebook.com/icko.gyorev/"
+          linked="https://www.linkedin.com/in/iskren-gyorev-4365b1239/"
+        />
       </Grid>
 
       <Grid item xs={4} sx={{ textAlign: 'center' }}>
         <h1>Gaming Forum</h1>
-        <p>
+        <p style={{ fontSize: '18px' }}>
           Our Forum project is part of Telerik Academy's JavaScript Alpha
-          program, a national IT group and leading IT teaching center. Visit
-          their corporate site. © Telerik Academy, 30 Krastyo Rakovski Str. 1729
-          Sofia, Bulgaria.
+          program, a national IT group and leading IT teaching center. <br />
+          <br />
+          Visit their corporate site. <br /> © Telerik Academy, 30 Krastyo
+          Rakovski Str. <br /> 1729 Sofia, Bulgaria.
+          <br />
           <br />
           All rights reserved.
         </p>
       </Grid>
 
       <Grid item xs={4}>
-        <Profile />
+        <Profile
+          name="Georgi Georgiev"
+          img={img2}
+          age="25"
+          location="https://www.google.com/maps/place/%D0%A1%D0%BE%D1%84%D0%B8%D1%8F/@42.695537,23.2539071,12z/data=!3m1!4b1!4m5!3m4!1s0x40aa8682cb317bf5:0x400a01269bf5e60!8m2!3d42.6977082!4d23.3218675"
+          town="Sofia"
+          ins="https://www.instagram.com/georgigeorgiev96/"
+          fb="https://www.facebook.com/georgi.georgiev.dancer/"
+          linked="https://www.linkedin.com/in/georgi-georgiev-071605203/"
+        />
       </Grid>
 
       <Grid item xs={12} sx={{ textAlign: 'center' }}>
@@ -35,6 +57,7 @@ const AboutPage = () => {
         <Rating
           name="simple-controlled"
           value={value}
+          size="large"
           onChange={(_, newValue) => {
             setValue(newValue);
           }}
--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -4,17 +4,15 @@ import './Profile.css';
 import { Container } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import { Divider } from '@mui/material';
-import profileImg from './imgs/iskren.JPG';
 
 import SchoolIcon from '@mui/icons-material/School';
 
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
-import YouTubeIcon from '@mui/icons-material/YouTube';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
-const Profile = () => {
+const Profile = ({ name, img, age, location, town, ins, fb, linked }) => {
   return (
     <Container
       className="profile-container"
@@ -31,41 +29,48 @@ const Profile = () => {
       <Grid container spacing={2} direction="column">
         <Grid item xs={12} sx={{ textAlign: 'center' }}>
           <img
-            src={profileImg}
+            src={img}
             style={{ width: '160px', height: '160px', objectFit: 'cover' }}
             alt="profile-pic"
           />
         </Grid>
         <Grid item xs={12} sx={{ textAlign: 'center' }}>
-          <h3>Iskren Gyorev</h3>
+          <h3>{name}</h3>
           <p style={{ fontStyle: 'italic' }}>
-            <SchoolIcon sx={{ color: '#47DB00', fontSize: 'medium' }} /> Telerik
-            Academy student
+            <a
+              href="https://www.telerikacademy.com/"
+              target="_blank"
+              rel="noreferrer"
+              style={{ textDecoration: 'none' }}
+            >
+              <SchoolIcon sx={{ color: '#47DB00', fontSize: 'medium' }} />{' '}
+            </a>
+            Telerik Academy student
           </p>
           <p style={{ marginTop: '15px' }}>About</p>
           <Divider />
         </Grid>
         <Grid item xs={12} sx={{ textAlign: 'center' }}>
-          <p>24 years old</p>
+          <p>{age} years old</p>
         </Grid>
         <Grid item xs={12} sx={{ textAlign: 'center' }}>
           <a
-            href="https://www.google.com/maps/place/%D0%92%D0%B8%D0%B4%D0%B8%D0%BD/@43.9796191,22.8423547,13z/data=!3m1!4b1!4m5!3m4!1s0x475379eca2009025:0x1f853ae2f877d8b7!8m2!3d43.996159!4d22.8679302"
+            href={location}
             target="_blank"
             rel="noreferrer"
             style={{ textDecoration: 'none' }}
           >
-            <LocationOnIcon /> Vidin, Bulgaria
+            <LocationOnIcon /> {town}, Bulgaria
           </a>
         </Grid>
         <Grid item textAlign="center">
-          <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+          <a href={ins} target="_blank" rel="noreferrer">
             <InstagramIcon />
           </a>
-          <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+          <a href={fb} target="_blank" rel="noreferrer">
             <FacebookIcon />
           </a>
-          <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+          <a href={linked} target="_blank" rel="noreferrer">
             <LinkedInIcon />
           </a>
         </Grid>
