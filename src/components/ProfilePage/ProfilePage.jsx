a924d0cb0bad8a676b18e56f1f220b0c24694a6b
Iskren Gyorev <iskrengyorev@gmail.com>
2022-05-04 16:55:49 +0300

    initial profile page

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,27 +1,121 @@
-import React from "react";
-import background from "../../assets/lR2zdL.jpg";
-import avatar from '../../assets/avatar.jpg';
-import "./ProfilePage.css";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
+import React, { useContext, useState } from 'react';
+import './ProfilePage.css';
+
+import {
+  ref as storageRef,
+  uploadBytes,
+  getDownloadURL,
+} from 'firebase/storage';
+import { storage } from '../../config/firebase-config';
+
+import { updateUserProfilePicture } from '../../services/users.service';
+
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import { Box, Button, Divider, Modal } from '@mui/material';
+
+import background from '../../assets/lR2zdL.jpg';
+import defaultAvatar from '../../assets/avatar.jpg';
+
 import EditIcon from '@mui/icons-material/Edit';
-import TopicRow from '../CategoryView/TopicRow/TopicRow'
+import TopicRow from '../CategoryView/TopicRow/TopicRow';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-function ProfilePage() {
 
-  
-function createData(title, content, author, date, replies, rating,comments) {
-  return {title, content, author, date, replies, rating,comments };
-}
+import AppContext from '../../providers/AppContext';
 
-const rows = [
-  createData( "Are we ever going to see Just Cause 5?"," YEAH I SAID IT! I don't know how much longer it's gonna take but i can't take it anymore. This game is just AMAZING! And now what? we prob won't see it again.YEAH I SAID IT! I don't know how much longer it's gonna take but i can't take it anymore. This game is just AMAZING! And now what? we prob won't see it again.YEAH I SAID IT! I don't know how much longer it's gonna take but i can't take it anymore. This game is just AMAZING! And now what? we prob won't see it again.YEAH I SAID IT! I don't know how much longer it's gonna take but i can't take it anymore. This game is just AMAZING! And now what? we prob won't see it again.YEAH I SAID IT! I don't know how much longer it's gonna take but i can't take it anymore. This game is just AMAZING! And now what? we prob won't see it again.YEAH I SAID IT! I don't know how much longer it's gonna take but i can't take it anymore. This game is just AMAZING! And now what? we prob won't see it again.", "thrills3eker101","31.05/2021", 67, 81),
-  createData( "WoW retri paladin build","IT's just the best i no.", "RetriOverProt", "8/08/2022",49, 3),
-  createData( "Where to go after final boss","Now i have no life...HALP ", "justLost", "8/11/2022",13, 21),
-  createData( "Best gaming moust for CS GO","Issi worth spending 200 dollers on am mouse", "DumbAi", "3/08/2022",9, 9),
-];
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
+const ProfilePage = () => {
+  const createData = (
+    title,
+    content,
+    author,
+    date,
+    replies,
+    rating,
+    comments
+  ) => {
+    return { title, content, author, date, replies, rating, comments };
+  };
 
+  const rows = [
+    createData(
+      'Are we ever going to see Just Cause 5?',
+      " YEAH I SAID IT! I don't know how much longer it's gonna take but i can't take it anymore. This game is just AMAZING! And now what? we prob won't see it again.YEAH I SAID IT! I don't know how much longer it's gonna take but i can't take it anymore. This game is just AMAZING! And now what? we prob won't see it again.YEAH I SAID IT! I don't know how much longer it's gonna take but i can't take it anymore. This game is just AMAZING! And now what? we prob won't see it again.YEAH I SAID IT! I don't know how much longer it's gonna take but i can't take it anymore. This game is just AMAZING! And now what? we prob won't see it again.YEAH I SAID IT! I don't know how much longer it's gonna take but i can't take it anymore. This game is just AMAZING! And now what? we prob won't see it again.YEAH I SAID IT! I don't know how much longer it's gonna take but i can't take it anymore. This game is just AMAZING! And now what? we prob won't see it again.",
+      'thrills3eker101',
+      '31.05/2021',
+      67,
+      81
+    ),
+    createData(
+      'WoW retri paladin build',
+      "IT's just the best i no.",
+      'RetriOverProt',
+      '8/08/2022',
+      49,
+      3
+    ),
+    createData(
+      'Where to go after final boss',
+      'Now i have no life...HALP ',
+      'justLost',
+      '8/11/2022',
+      13,
+      21
+    ),
+    createData(
+      'Best gaming moust for CS GO',
+      'Issi worth spending 200 dollers on am mouse',
+      'DumbAi',
+      '3/08/2022',
+      9,
+      9
+    ),
+  ];
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const { user, userData, setContext } = useContext(AppContext);
+  console.log(userData);
+  const username = userData?.username;
+
+  const uploadPicture = (e) => {
+    e.preventDefault();
+
+    const file = e.target[0]?.files?.[0];
+
+    if (!file) return alert(`Please select a file!`);
+
+    const picture = storageRef(storage, `images/${username}/avatar`);
+
+    uploadBytes(picture, file)
+      .then((snapshot) => {
+        return getDownloadURL(snapshot.ref).then((url) => {
+          return updateUserProfilePicture(username, url).then(() => {
+            setContext({
+              user,
+              userData: {
+                ...userData,
+                avatarUrl: url,
+              },
+            });
+          });
+        });
+      })
+      .catch(console.error);
+  };
 
   return (
     <div className="profileContainer">
@@ -32,38 +126,116 @@ const rows = [
       <Container
         maxWidth="lg"
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          height: "auto",
-          backgroundColor: "white",
-          boxShadow: "0 1px 6px rgba(0,0,0,0.2)",
-          marginTop: "2rem",
-          marginBottom: "2rem",
-          paddingBottom: "1rem",
+          height: 'auto',
+          backgroundColor: 'white',
+          boxShadow: '0 1px 6px rgba(0,0,0,0.2)',
+          marginTop: '50px',
+          paddingBottom: '55px',
         }}
       >
-          <div className="profileRow">
+        <Grid
+          container
+          direction="column"
+          spacing={1}
+          sx={{ textAlign: 'center' }}
+        >
+          <Grid item>
+            {userData.avatarUrl ? (
+              <img src={userData.avatarUrl} alt="profile" />
+            ) : (
+              <img src={defaultAvatar} alt="profile"></img>
+            )}
+          </Grid>
 
-            <img src={avatar} alt="avatar" className="avatar"></img><EditIcon />
-          </div>
-         
-            <h1>Mr. AdminButNotReallyAdminGuy</h1>
-            <h3>This is my super awesome description that i can also change <EditIcon /></h3>
-            
-            <h1>My Posts:</h1>
-            <div className="profilePosts">
-      {rows.map((row) => (
-      <Grid sx={{marginTop: "0.5rem", display: "flex", flexDirection: "row"}} >         
-       <TopicRow row={row}/>
-       <div className="topicEditDelete"><EditIcon /> <DeleteForeverIcon /> </div>
-        
-      </Grid>
-     ))}
-     </div>
+          <Grid>
+            <a
+              onClick={handleOpen}
+              style={{
+                color: 'black',
+                fontSize: '13.5px',
+                textTransform: 'none',
+                cursor: 'pointer',
+              }}
+            >
+              Change picture <EditIcon />
+            </a>
+          </Grid>
+
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <div
+                style={{
+                  display: 'flex',
+                  top: '-50px',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                }}
+              >
+                <h4>Upload a picture</h4>
+                <Divider sx={{ bgcolor: '#47DB00' }} />
+
+                <button
+                  style={{
+                    background: 'none',
+                    border: 'none',
+                    fontSize: '20px',
+                    fontWeight: 'bold',
+                    cursor: 'pointer',
+                  }}
+                  onClick={handleClose}
+                >
+                  X
+                </button>
+              </div>
+
+              <div>
+                <form onSubmit={uploadPicture}>
+                  <input type="file" name="file"></input>
+                  <button type="submit">Submit</button>
+                </form>
+              </div>
+            </Box>
+          </Modal>
+
+          <Grid item>
+            <h1>{userData.username}</h1>
+          </Grid>
+
+          <Grid item>
+            <p style={{ marginBottom: '24px' }}>
+              This is my super awesome description that i can also change{' '}
+              <EditIcon />
+            </p>
+          </Grid>
+
+          <Divider />
+        </Grid>
+        <h1>My Posts:</h1>
+
+        <div className="profilePosts">
+          {rows.map((row) => (
+            <Grid
+              sx={{
+                marginTop: '0.5rem',
+                display: 'flex',
+                flexDirection: 'row',
+              }}
+            >
+              <TopicRow row={row} />
+              <div className="topicEditDelete">
+                <EditIcon /> <DeleteForeverIcon />{' '}
+              </div>
+            </Grid>
+          ))}
+        </div>
       </Container>
     </div>
   );
-}
+};
 
 export default ProfilePage;
--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -2,26 +2,21 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage } from 'firebase/storage';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyCqolKvhxzuDMW38pbbP-GergQW_O8GL34",
-  authDomain: "react-team3-forum.firebaseapp.com",
-  projectId: "react-team3-forum",
-  storageBucket: "react-team3-forum.appspot.com",
-  messagingSenderId: "602768559676",
-  appId: "1:602768559676:web:47c92e222fe0f90f7ebd39",
-  measurementId: "G-L8K310Z1J2",
-  databaseURL: "https://react-team3-forum-default-rtdb.europe-west1.firebasedatabase.app/"
+  apiKey: 'AIzaSyCqolKvhxzuDMW38pbbP-GergQW_O8GL34',
+  authDomain: 'react-team3-forum.firebaseapp.com',
+  projectId: 'react-team3-forum',
+  storageBucket: 'react-team3-forum.appspot.com',
+  messagingSenderId: '602768559676',
+  appId: '1:602768559676:web:47c92e222fe0f90f7ebd39',
+  measurementId: 'G-L8K310Z1J2',
+  databaseURL:
+    'https://react-team3-forum-default-rtdb.europe-west1.firebasedatabase.app/',
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-// the Firebase authentication handler
 export const auth = getAuth(app);
-// the Realtime Database handler
 export const db = getDatabase(app);
+export const storage = getStorage(app);
