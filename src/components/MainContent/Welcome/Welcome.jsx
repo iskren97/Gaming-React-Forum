0821006a2535ad7a69d660f22bc1dffe20eb79b7
Iskren Gyorev <iskrengyorev@gmail.com>
2022-05-10 21:53:31 +0300

    Updated welcome functionality

--- src/components/MainContent/Welcome/Welcome.jsx
+++ src/components/MainContent/Welcome/Welcome.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import logo from './telerik.svg';
 import './Welcome.css';
 import LoginModal from '../../JoinCommunityModal/JoinModal';
+import AppContext from '../../../providers/AppContext';
+import { useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Welcome = () => {
+  const { user, userData } = useContext(AppContext);
+  const navigate = useNavigate();
+
   return (
     <div className="welcome-section">
       <div className="inner-welcome-section">
         <div className="media">
-          <h1>Welcome to the forum!</h1>
-          <a href="#/" className="join-button">
-            <LoginModal />
-          </a>
+          {user ? (
+            <div>
+              <h1>Welcome,</h1>
+
+              <NavLink to={`/profile/${userData.username}`}>
+                <h2 className="user-username" data-replace={userData.username}>
+                  <span>{userData.username}</span>
+                </h2>
+              </NavLink>
+            </div>
+          ) : (
+            <div>
+              <h1>Welcome to the forum!</h1>
+              <a href="#/" className="join-button">
+                <LoginModal />
+              </a>
+            </div>
+          )}
         </div>
       </div>
 
